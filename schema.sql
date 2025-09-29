02421c1f7430300a56a37ba5194337a3731ab6a6
Ethan Kuo <ethankuocodes@gmail.com>
2025-09-28 23:56:47 -0400

    update schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
- CREATE TABLE logs (
+CREATE TABLE logs (
     id                  SERIAL PRIMARY KEY,
     name                TEXT NOT NULL,
     date                DATE NOT NULL,
     start_time          TIME NOT NULL,
     duration            INTERVAL NOT NULL,
     description         TEXT,
-    distractions        INTEGER DEFAULT 0 CHECK (distractions >= 0),
-    context_switches    INTEGER DEFAULT 0 CHECK (context_switches >= 0)
+    sub_tasks           INTEGER DEFAULT 1 CHECK (sub_tasks >= 1)
 );
